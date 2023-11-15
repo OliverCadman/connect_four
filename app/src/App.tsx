88cdf8825ae0d5a452a24d11802830d3d9c7ac51
Oliver Cadman <o.cadman@live.co.uk>
2023-11-15 14:45:27 +0000

    Remove unused imports

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,9 +2,7 @@ import { AppStateProvider } from "./context/AppStateContext";
 import { GameStateProvider } from "./context/GameDataContext";
 
 import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
 import Router from "./router/router";
-import Home from "./pages/Home";
 
 function App() {
   return (
